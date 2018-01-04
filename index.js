9dae337bf50c13cb80cf324729cf7e38ac7d8942
Mika Äärilä <maarila@gmail.com>
2018-01-04 14:22:31 +0200

    herokuun

--- index.js
+++ index.js
@@ -105,6 +105,7 @@ let notes = [
 //   response.end(JSON.stringify(notes));
 // });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+});
